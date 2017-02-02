951619ef4366a7c50dfc22aae0bae41419fb67ce
liuz6 <liuz6@7d43bf38-9a78-4e6f-8974-39d3906da97d>
2017-02-02 00:50:37 +0000

    new size

--- ArcadeGameProject/src/Hero.java
+++ ArcadeGameProject/src/Hero.java
@@ -1,4 +1,5 @@
 import java.applet.Applet;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
@@ -60,6 +61,16 @@ public class Hero extends Applet implements KeyListener {
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
+
+	}
+
+	public void drawOn(Graphics2D g2) {
+
+		g2.setColor(Color.blue);
+		rect = new Rectangle(0, 0, 50, 50);
+		rect2 = new Rectangle(0, 0, 20, 20);
+		g2.fill(rect);
+		g2.fill(rect2);
 
 	}
 
--- ArcadeGameProject/src/World.java
+++ ArcadeGameProject/src/World.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -18,33 +20,37 @@ public class World {
 	public void load() throws IOException {
 
 		JFrame myWindow = new JFrame();
-		myWindow.setSize(1000, 750);
+		myWindow.setSize(1000, 1000);
 		myWindow.setTitle("BomberMan");
 		myWindow.add(new WorldComponent());
 
-		// File inputFile = new File(level);
-		// Scanner inScanner = new Scanner(inputFile);
-		// while (inScanner.hasNext()) {
-		// String next = inScanner.next();
-		// if (next == " ") {
-		// return;
-		// } else if (next == "w") {
-		// return;
-		// } else if (next == "h") {
-		// return;
-		// } else if (next == "e") {
-		// return;
-		// } else if (next == "b") {
-		// return;
-		// } else {
-		// throw new RuntimeException("Invalid Character in World Text File");
-		//
-		// }
-		// }
+		Hero myHero = new Hero();
+		myWindow.add(myHero);
+
+		File inputFile = new File(level);
+		Scanner inScanner = new Scanner(inputFile);
+		while (inScanner.hasNext()) {
+			String next = inScanner.next();
+			if (next == " ") {
+				return;
+			} else if (next == "w") {
+				return;
+			} else if (next == "h") {
+				return;
+			} else if (next == "e") {
+				return;
+			} else if (next == "b") {
+				return;
+			} else {
+				// throw new RuntimeException("Invalid Character in World Text
+				// File");
+
+			}
+		}
 
 		myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myWindow.setVisible(true);
-		// inScanner.close();
+		inScanner.close();
 	}
 
 }
