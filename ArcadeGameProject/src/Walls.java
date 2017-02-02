83548a4243e46dd9b4024c8d7471900dd2ee23d0
robisoct <robisoct@7d43bf38-9a78-4e6f-8974-39d3906da97d>
2017-02-02 01:51:51 +0000

    Created constructor for walls and drawOn method

---
+++ ArcadeGameProject/src/Walls.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public class Walls {
+	
+	private Rectangle rect;
+	private double rectCenterX;
+	private double rectCenterY;
+	private int side = 50;
+	private Color rectColor = Color.GRAY;
+	
+	public Walls(int x, int y) {
+		rect.height = side;
+		rect.width = side;
+		rectCenterX = x * (side/2);
+		rectCenterY = 250 + (y + (side/2));
+	}
+	
+	public void drawOn(Graphics2D graphics2) {
+		graphics2.setColor(rectColor);
+		graphics2.fillRect(
+				(int)rectCenterX, 
+				(int)rectCenterY, 
+				side, 
+				side);
+	}
+}
