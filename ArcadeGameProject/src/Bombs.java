3c412cf58ac61e1a478a2355be38a5f22e2b10a3
sanchede <sanchede@7d43bf38-9a78-4e6f-8974-39d3906da97d>
2017-02-05 03:34:03 +0000

    Working on Explosion of bombs

--- ArcadeGameProject/src/Bombs.java
+++ ArcadeGameProject/src/Bombs.java
@@ -12,6 +12,7 @@ public class Bombs extends JComponent {
 	private Color color;
 	private Hero myHero;
 
+
 	public Bombs(Hero myHero) {
 
 		this.myBomb = new Rectangle(0, 0, 0, 0);
@@ -30,9 +31,18 @@ public class Bombs extends JComponent {
 
 	}
 
-	public void drop() {
+	public void drop()  {
 		x = myHero.getX();
 		y = myHero.getY();
+		this.explode();
+		
+	}
+
+	public boolean explode() {
+		this.myBomb.grow(3, 3);
+		this.myBomb.setSize(0, 0);	
+		this.repaint();
+		return true;
 	}
 
 }
