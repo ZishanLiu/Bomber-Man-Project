3f3d489645e95fec5c6e64bf2ba7eb303ca720a1
sanchede <sanchede@7d43bf38-9a78-4e6f-8974-39d3906da97d>
2017-02-15 02:39:38 +0000

    Added powerup interface

---
+++ ArcadeGameProject/src/PowerUps.java
@@ -0,0 +1,12 @@
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public interface PowerUps {
+	public boolean getPowerup();
+
+	public void set(int x, int y);
+
+	public Rectangle getRect();
+
+	public void drawOn(Graphics2D g2);
+}
