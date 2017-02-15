cce83963568317754918ac8fde7d0cd489b3c8c2
sanchede <sanchede@7d43bf38-9a78-4e6f-8974-39d3906da97d>
2017-02-15 02:41:00 +0000

    2nd power up

---
+++ ArcadeGameProject/src/BombIncrease.java
@@ -0,0 +1,54 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public class BombIncrease {
+	private Rectangle moreBomb;
+	private Hero myHero;
+
+	private int RectX = 240;
+	private int RectY = 287;
+	private int grid = 50;
+	private int side = 25;
+	private WorldComponent myComponent;
+
+	public BombIncrease(Hero myHero, WorldComponent myComponent) {
+		this.moreBomb = moreBomb;
+		this.myHero = myHero;
+		this.myComponent = myComponent;
+	}
+	public void drawOn(Graphics2D g2) {
+
+		Graphics2D g = (Graphics2D) g2;
+		g.setColor(Color.WHITE);
+
+		moreBomb = new Rectangle(RectX, RectY, side, side);
+
+		g.fill(moreBomb);
+	}
+
+	public Rectangle getRect() {
+
+		return new Rectangle(RectX, RectY, side, side);
+
+	}
+
+	public void set(int x, int y) {
+
+		this.RectX = (x * grid) + grid / 2 + 10;
+		this.RectY = (y * grid) + grid / 2 + 10;
+
+	}
+
+	public boolean getPowerup() {
+
+		if (myHero.getBounds2D().intersects(this.getRect())) {
+			side = 0;
+			System.out.println("big");
+			return true;
+
+		}
+
+		return false;
+	}
+}
