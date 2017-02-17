0a39809de6908d9fa2f001a0eb4ebe4143704156
robisoct <robisoct@7d43bf38-9a78-4e6f-8974-39d3906da97d>
2017-02-17 00:43:10 +0000

    myListener code commented for presentation

--- ArcadeGameProject/src/myListener.java
+++ ArcadeGameProject/src/myListener.java
@@ -17,46 +17,37 @@ public class myListener implements KeyListener {
 		// this.myMonster = myMonster;
 	}
 
+	/*
+	 * Function that waits for the user to press set of keys for movement of the hero.
+	 * 
+	 * @param e - KeyEvent object that is passed in to function
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			myHero.moveRight();
-
 		}
-
 		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 			myHero.moveLeft();
-
 		}
-
 		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 			myHero.moveDown();
-
 		}
-
 		if (e.getKeyCode() == KeyEvent.VK_UP) {
 			myHero.moveUp();
-
 		}
 		for (int i = 0; i < game.Monsters.size(); i++) {
 			game.Monsters.get(i).checkHero();
 		}
-
 		if (!game.hero.checkContact()) {
 			startX = myHero.getX();
 			startY = myHero.getY();
-
 		} else {
 			myHero.stuck(startX, startY);
-
 		}
-
 		if (game.rangeBomb.getPowerup()) {
-
 			System.out.println("get");
-
 		}
-
 	}
 
 	@Override
