e55f4ae47e58cdc00210e7e2721e15c60c31ba76
robisoct <robisoct@7d43bf38-9a78-4e6f-8974-39d3906da97d>
2017-02-17 00:27:07 +0000

    LargerRangeBomb code has been commented for presentation

--- ArcadeGameProject/src/LargerRangeBomb.java
+++ ArcadeGameProject/src/LargerRangeBomb.java
@@ -20,45 +20,47 @@ public class LargerRangeBomb implements PowerUps{
 
 	}
 
+	/*
+	 * Function that draws the powerup pickup for larger ranged bombs.
+	 * 
+	 * @param g2 - graphics object on which to draw
+	 */
 	public void drawOn(Graphics2D g2) {
-
 		Graphics2D g = (Graphics2D) g2;
 		g.setColor(Color.WHITE);
-
 		RangeBomb = new Rectangle(RectX, RectY, side, side);
-
 		g.fill(RangeBomb);
 	}
 
 	public Rectangle getRect() {
-
 		return new Rectangle(RectX, RectY, side, side);
-
 	}
 
+	/*
+	 * Function that sets the location of the powerup.
+	 * 
+	 * @param x - coordinate placement
+	 * 		y - coordinate placement
+	 */
 	public void set(int x, int y) {
-
 		this.RectX = (x * grid) + grid / 2 + 10;
 		this.RectY = (y * grid) + grid / 2 + 10;
-
 	}
 
+	/*
+	 * Function that checks to see if the hero has collided with the powerup.
+	 */
 	public boolean getPowerup() {
-
 		if (myHero.getBounds2D().intersects(this.getRect())) {
 			myComponent.increaseRange();
 			side = 0;
 			System.out.println("big");
 			return true;
-
 		}
-
 		return false;
 	}
 
 	public void setHero(Hero hero) {
 		this.myHero = hero;
-		
 	}
-
 }
