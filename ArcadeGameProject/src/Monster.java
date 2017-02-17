d76584aab19965ffd9cdb1e425344eb75f2324b5
robisoct <robisoct@7d43bf38-9a78-4e6f-8974-39d3906da97d>
2017-02-17 00:39:45 +0000

    Monster code has been commented for presentation

--- ArcadeGameProject/src/Monster.java
+++ ArcadeGameProject/src/Monster.java
@@ -33,41 +33,63 @@ public class Monster {
 
 		this.RectX = (x * grid) + grid / 2;
 		this.RectY = (y * grid) + grid / 2;
-
 	}
 
+	/*
+	 * Function that draws the monster on our screen
+	 * 
+	 * @param g2 - graphics object on which to draw
+	 */
 	public void drawOn(Graphics2D g2) {
-
 		Graphics2D g = (Graphics2D) g2;
 		g.setColor(Color.RED);
-
 		Rectangle myMonster = new Rectangle((int) RectX, (int) RectY, 20, 30);
-
 		g.fill(myMonster);
 	}
 
+	/*
+	 * Function that sets the location of the monster initially.
+	 * 
+	 * @param x - coordinate placement
+	 * 		y - coordinate placement
+	 */
 	public void set(int x, int y) {
-
 		this.RectX = (x * grid) + grid / 2 + 10;
 		this.RectY = (y * grid) + grid / 2 + 10;
-
 	}
 
+	/*
+	 * Function that returns the 'x' position of the monster.
+	 */
 	public int getX() {
 		return RectX;
 	}
 
+	/*
+	 * Function that returns the 'y' position of the monster.
+	 */
 	public int getY() {
 		return RectY;
 	}
 
+	/*
+	 * Function that resets the monster based on pixels rather than prior coordinate system.
+	 * 
+	 * @param x - pixel coordinate placement
+	 * 		y - pixel coordinate placement
+	 */
 	public void stuck(int x, int y) {
-
 		this.RectX = x;
 		this.RectY = y;
-
 	}
 
+	/*
+	 * Function that defines the movement of the monster.
+	 * 
+	 * @param x - change of speed in x direction
+	 * 		y - change of speed in y direction
+	 * 		direction - the direction (-1 or 1)
+	 */
 	public void move(int x, int y, int direction) {
 		moveYby = moveYby * direction;
 		moveXby = moveXby * direction;
@@ -79,6 +101,9 @@ public class Monster {
 		return new Rectangle(RectX, RectY, side, side);
 	}
 
+	/*
+	 * Function that checks for contact with walls of both kinds.
+	 */
 	public boolean checkContact() {
 		for (Walls walli : WI) {
 			if (walli.getRect().intersects(this.getBounds2D())) {
@@ -89,7 +114,6 @@ public class Monster {
 			if (wallb.getRect().intersects(this.getBounds2D())) {
 				return true;
 			}
-
 		}
 		return false;
 	}
@@ -99,18 +123,22 @@ public class Monster {
 		return new Rectangle(RectX, RectY, 20, 30);
 
 	}
-
+	
+	/*
+	 * Function that deals with the death of a monster.
+	 */
 	public void die() {
-
 		this.RectX = -50;
 		this.RectY = -50;
-
 	}
 
 	public void setHero(Hero hero) {
 		this.myhero = hero;
 	}
 
+	/*
+	 * Checks to see if the monster kills the hero by colliding with the hero.
+	 */
 	public boolean checkHero() {
 		try {
 			if (myhero.getBounds2D().intersects(this.getRect())) {
@@ -121,20 +149,14 @@ public class Monster {
 		} catch (IOException exception) {
 			throw new RuntimeException("failed checking hero vs monster");
 		}
-
 		return false;
 	}
 
 	public void disappear() {
-
 		this.side = 0;
-
 	}
 
 	public int getType() {
-
 		return this.type;
-
 	}
-
 }
